c4baeb9481554484b0786282ccadc051095abe94
NarcissusTu <763030205@qq.com>
2025-12-26 12:29:55 +0800

    change build.rs to build static library

--- build.zig
+++ build.zig
@@ -4,53 +4,29 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("zigs", .{
+    // 1. 先创建一个共享的 root_module，这样库和测试都能共用一套配置
+    const common_module = b.createModule(.{
         .root_source_file = b.path("src/lib.zig"),
-
-        .target = target,
+        .target = target, // 必须在这里！
+        .optimize = optimize, // 必须在这里！
     });
 
-    const exe = b.addExecutable(.{
-        .name = "zigs",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            // List of modules available for import in source files part of the
-            // root module.
-            .imports = &.{
-                .{ .name = "zigs", .module = mod },
-            },
-        }),
+    // 2. 定义库
+    const lib = b.addLibrary(.{
+        .name = "colored",
+        .root_module = common_module,
+        .linkage = .static,
+    });
+    b.installArtifact(lib);
+
+    // 3. 配置单元测试
+    // 同样直接引用上面那个已经包含 target 的 common_module
+    const lib_unit_tests = b.addTest(.{
+        .root_module = common_module,
     });
 
-    b.installArtifact(exe);
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    const run_step = b.step("run", "Run the app");
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_step.dependOn(&run_cmd.step);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
-
-    // A run step that will run the test executable.
-    const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
 }
